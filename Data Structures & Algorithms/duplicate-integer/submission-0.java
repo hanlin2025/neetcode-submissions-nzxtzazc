d6754ee3828cd318117e40bab62670f22a1090f9
Awu <hanlin.wu.2025@computing.smu.edu.sg>
2026-04-04 18:24:17 +0800

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        boolean output = false;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    output = true;
+                    break;
+                }
+            }
+            if (output) {
+                break;
+            }
+        }
+
+        return output;
+    }
+}
