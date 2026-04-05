b25343893c5e46403665cc3eb0658124ad161670
Awu <hanlin.wu.2025@computing.smu.edu.sg>
2026-04-05 16:03:22 +0800

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer> rList = new ArrayList<>();
+        
+        int[][] indexes = new int[nums.length][2];
+
+        for (int i = 0; i < nums.length; i++) {
+            indexes[i][0] = nums[i];
+            indexes[i][1] = i;
+        }
+
+        Arrays.sort(indexes, (a,b) -> Integer.compare(a[0], b[0]));
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (left == right) {
+                break;
+            }
+            if (indexes[left][0] + indexes[right][0] == target) {
+                rList.add(indexes[left][1]);
+                rList.add(indexes[right][1]);
+                break;
+            } else if (indexes[left][0] + indexes[right][0] > target) {
+                right--;
+            } else if (indexes[left][0] + indexes[right][0] < target) {
+                left++;
+            } 
+        }
+
+        Collections.sort(rList);
+        int[] rArray = new int[rList.size()];
+        for (int i = 0; i < rList.size(); i++) {
+            rArray[i] = rList.get(i);
+        }
+
+        return rArray;
+    }
+}
